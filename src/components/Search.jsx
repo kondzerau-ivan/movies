3ca4678bb5cc8be083c1315f1a08deb02e6da982
konderev <konderev@raveinfosys.com>
2021-05-05 19:57:08 +0300

    add button search and start work on filters

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -20,7 +20,7 @@ class Search extends React.Component {
   render() {
     return (
       <div className="row">
-        <div className="input-field col s12">
+        <div className="input-field">
           <input type="text"
                  placeholder="Movie you are looking for"
                  value={this.state.search}
@@ -28,6 +28,21 @@ class Search extends React.Component {
                  onChange={(event) => {this.setState({search: event.target.value})}}
                  onKeyDown={this.handleKey}
           />
+          <button className="btn" onClick={() => this.props.searchMovies(this.state.search)}>Search</button>
+        </div>
+        <div className="row">
+          <label className="col s4 label-type">
+            <input className="with-gap radio" name="group3" type="radio" checked/>
+            <span className="span">All</span>
+          </label>
+          <label className="col s4 label-type">
+            <input className="with-gap radio" name="group3" type="radio" checked/>
+            <span className="span">Movies</span>
+          </label>
+          <label className="col s4 label-type">
+            <input className="with-gap radio" name="group3" type="radio" checked/>
+            <span className="span">Series</span>
+          </label>
         </div>
       </div>
     );
