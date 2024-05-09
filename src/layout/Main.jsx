e9a994d9cda7e073e6e02d5517760d147e4239f0
Ivan Konderev <konderev@aheadworks.com>
2024-05-09 22:30:59 +0300

    Add Search component

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Movies  from "../components/Movies";
+import Spinner from "../components/Spinner";
+import Search from "../components/Search";
 
 export default class Main extends React.Component {
   state = {
@@ -12,16 +14,23 @@ export default class Main extends React.Component {
       .then(data => this.setState({movies: data.Search}));
   }
 
+  searchMovies = (str) => {
+    fetch(`http://www.omdbapi.com/?apikey=a8c73077&s=${str}`)
+      .then(response => response.json())
+      .then(data => this.setState({movies: data.Search}));
+  }
+
   render() {
     const {movies} = this.state;
 
     return(
       <main>
-        <div className="container mx-auto px-6 pb-44">
+        <div className="container mx-auto p-6 pb-44">
+          <Search searchMovies={this.searchMovies} />
           {
             movies.length ?
             <Movies movies={movies} /> :
-            <h2>Loading...</h2>
+            <Spinner />
           }
           
         </div>
