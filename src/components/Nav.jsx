bdd7697d1cd3bd5466f531bf3055ef8d04d401cc
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2024-05-11 01:01:35 +0300

    Add link to Repo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,7 +12,7 @@ export default function Nav() {
         <ul className="flex flex-col gap-2 my-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
           <li className="block p-1 font-sans text-sm antialiased font-medium leading-normal text-blue-gray-900">
             <a
-              href="#"
+              href="https://github.com/kondzerau-ivan/movies"
               className="flex items-center transition-colors hover:text-blue-500"
             >
               Repo
